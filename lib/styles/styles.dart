f6d7c7516c5041d7c7aeb8a27b6127f971cb2eb6
DeployChef <dc.titovvlad@gmail.com>
2022-02-14 16:00:02 +0300

    colorBlackGrey

--- lib/styles/styles.dart
+++ lib/styles/styles.dart
@@ -2,7 +2,7 @@ import 'package:flutter/widgets.dart';
 import 'package:places/styles/color_constants.dart';
 
 const largeTitleStyle = TextStyle(
-  color: colorWhite,
+  color: colorBlackGrey,
   fontFamily: 'Roboto',
   fontSize: 32.0,
   height: 1.125,
