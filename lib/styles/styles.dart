018a51eb8e8e752574dbbe2ead2149b441b53f71
DeployChef <dc.titovvlad@gmail.com>
2022-02-14 14:45:59 +0300

    добавил апп бар список интересных мест

---
+++ lib/styles/styles.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'package:places/styles/color_constants.dart';
+
+const largeTitleStyle = TextStyle(
+  color: colorWhite,
+  fontFamily: 'Roboto',
+  fontSize: 32.0,
+  height: 1.125,
+  fontWeight: FontWeight.w700,
+);
