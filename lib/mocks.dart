905e69274cca08af9479745873f1949df448b9ed
Vladislav <dc.titovvlad@gmail.com>
2022-02-16 17:26:59 +0300

    add mock data

---
+++ lib/mocks.dart
@@ -0,0 +1,31 @@
+import 'package:places/domain/sight.dart';
+
+final List mocks = <Sight>[
+  Sight(
+    name: 'Краеведческий музей',
+    lat: 11.111111,
+    lon: 22.222222,
+    url: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Краеведческий_музей_%281%29.jpg',
+    type: 'музей',
+    details:
+        'Музей, объектом деятельности которого является документация',
+  ),
+  Sight(
+    name: 'Памятник ленину (Ногинск)',
+    lat: 33.333333,
+    lon: 22.222222,
+    url: 'https://vsedomarossii.ru/photos/area_50/city_2225/street_3853/_20463_1.jpg',
+    type: 'Памятник',
+    details:
+        'Первый памятник ленину',
+  ),
+  Sight(
+    name: 'Краеведческий музей',
+    lat: 11.111111,
+    lon: 22.222222,
+    url: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Краеведческий_музей_%281%29.jpg',
+    type: 'музей',
+    details:
+        'Музей, объектом деятельности которого является документация',
+  ),
+];
