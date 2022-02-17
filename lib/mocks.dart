c52f465ccc32c6aba24b9305f0ea09c32f9426e6
DeployChef <dc.titovvlad@gmail.com>
2022-02-17 22:46:48 +0300

    Длиннее слово

--- lib/mocks.dart
+++ lib/mocks.dart
@@ -2,13 +2,12 @@ import 'package:places/domain/sight.dart';
 
 final List<Sight> mocks = <Sight>[
   Sight(
-    name: 'Краеведческий музей',
+    name: 'Воронежский областной краеведческий музей',
     lat: 11.111111,
     lon: 22.222222,
     url: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Краеведческий_музей_%281%29.jpg',
     type: 'музей',
-    details:
-        'Музей, объектом деятельности которого является документация',
+    details: 'Музей, объектом деятельности которого является документация',
   ),
   Sight(
     name: 'Памятник ленину (Ногинск)',
@@ -16,8 +15,7 @@ final List<Sight> mocks = <Sight>[
     lon: 22.222222,
     url: 'https://vsedomarossii.ru/photos/area_50/city_2225/street_3853/_20463_1.jpg',
     type: 'Памятник',
-    details:
-        'Первый памятник ленину',
+    details: 'Первый памятник ленину',
   ),
   Sight(
     name: 'Краеведческий музей',
@@ -25,7 +23,6 @@ final List<Sight> mocks = <Sight>[
     lon: 22.222222,
     url: 'https://upload.wikimedia.org/wikipedia/commons/4/49/Краеведческий_музей_%281%29.jpg',
     type: 'музей',
-    details:
-        'Музей, объектом деятельности которого является документация',
+    details: 'Музей, объектом деятельности которого является документация',
   ),
 ];
