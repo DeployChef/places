38e24af6c7fbf2817bcb648356d57568380ad063
Vladislav <dc.titovvlad@gmail.com>
2022-06-05 13:37:42 +0300

    setup dark theme on dark

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class App extends StatelessWidget {
       ],
       title: 'Places',
       theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.lightTheme,
+      darkTheme: AppTheme.darkTheme,
       home: VisitingScreen(), //,
     );
   }
